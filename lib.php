77abcba4ee2fae02f70c7c84611af3af476865b4
i3elj <guisande.bernardo@gmail.com>
2023-11-24 20:28:36 -0300

    library file for helper functions

---
+++ lib.php
@@ -0,0 +1,22 @@
+<?php
+
+function dd(...$args)
+{
+      echo "<pre>";
+      var_dump(...$args);
+      echo "</pre>";
+      die();
+}
+
+function require_page($path)
+{
+      require_once $path;
+      exit(0);
+}
+
+function _404()
+{
+      http_response_code(404);
+      echo "<h1>404 Page Not Found</h1>";
+      exit(0);
+}
